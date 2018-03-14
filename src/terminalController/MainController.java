52d0af05f32681d6b2e52af66e7dd9322d7c0b99
rosvik <j.rosvik@gmail.com>
2018-03-14 13:53:29 +0100

    Updated terminal controller

--- src/terminalController/MainController.java
+++ src/terminalController/MainController.java
@@ -2,16 +2,50 @@ package terminalController;
 
 public class MainController {
 
-	public MainController() {
-		System.out.println("Hello World");
+	private String command = null;
+
+	private MainController(String[] args) {
+		if (args.length == 0) {
+			System.out.println("No argument specified, see 'help' for usage instructions.");
+		} else {
+			boolean isFunc = testForFunc(args[0]);
+			if (isFunc) {
+				if (this.command.equals("add")) {
+					System.out.println("TODO: 'Add' function"); // TODO: 'Add' function
+				} else if (this.command.equals("list")) {
+					System.out.println("TODO: 'List' function"); // TODO: 'List' function
+				} else if (this.command.equals("connect")) {
+					System.out.println("TODO: 'Connect' function"); // TODO: 'Connect' function
+				} else if (this.command.equals("help")) {
+					printHelp();
+				}
+			} else {
+				System.out.println("Unknown argument, see 'help' for usage instructions.");
+			}
+		}
 	}
 
-	public void hello() {
-		System.out.println("¯\\_(ツ)_/¯");
+	private boolean testForFunc(String arg) {
+		if (arg.equals("help")) {
+//			printHelp();
+			this.command = "help";
+			return true;
+		} else if (arg.equals("add")) {
+			this.command = "add";
+			return true;
+		}
+		return false;
+	}
+
+	private void printHelp() {
+		System.out.println("THIS IS HOW YOU DO:");
+		System.out.println("help:     See this, duh.");
+		System.out.println("add:      See this, duh.");
+		System.out.println("list:     See this, duh.");
+		System.out.println("connect:  See this, duh.");
 	}
 
 	public static void main(String[] args) {
-		MainController mainController = new MainController();
-		mainController.hello();
+		MainController mainController = new MainController(args);
 	}
 }
