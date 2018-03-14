1b3df58ccb0d1eaedc064f03fc2fdb5b796ef36b
rosvik <j.rosvik@gmail.com>
2018-03-14 11:39:17 +0100

    First artifact build test

---
+++ src/terminalController/MainController.java
@@ -0,0 +1,17 @@
+package terminalController;
+
+public class MainController {
+
+	public MainController() {
+		System.out.println("Hello World");
+	}
+
+	public void hello() {
+		System.out.println("¯\\_(ツ)_/¯");
+	}
+
+	public static void main(String[] args) {
+		MainController mainController = new MainController();
+		mainController.hello();
+	}
+}
