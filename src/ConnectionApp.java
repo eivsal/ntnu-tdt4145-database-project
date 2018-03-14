700f988ce4dc55c8a34296ff9e458be66be16e4e
dennisjl <dennisjl@stud.ntnu.no>
2018-03-14 11:14:17 +0100

    Adds JDBC connectivity. Defines connection, returns connection object

---
+++ src/ConnectionApp.java
@@ -0,0 +1,35 @@
+//class for testing methods
+
+import sample.com.util.ConnectionConfiguration;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionApp {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+
+        try{
+            connection = ConnectionConfiguration.getConnection();
+            if(connection !=null) {
+                System.out.println("Connection established");
+            }
+        }
+        catch(Exception exc){
+            exc.printStackTrace();
+        }
+        finally{
+            if(connection !=null){
+                try{
+                    connection.close();
+                }
+                catch(SQLException exc){
+                    exc.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
---
+++ src/sample/com/util/ConnectionConfiguration.java
@@ -0,0 +1,34 @@
+//defining the connection
+//return connection object
+//every time we need to connect to mysql, we use this method
+
+package sample.com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class ConnectionConfiguration {
+
+    public static Connection getConnection(){
+        Connection connection = null;
+
+        //1. try to connect to the sql database
+        try{
+            //pointer to driver mysql connector
+            Class.forName("com.mysql.jdbc.Driver");
+            //retrieving my connection object
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demojdbc", "root", "root");
+
+        }
+        catch(Exception exc){
+            exc.printStackTrace();
+        }
+
+        return connection;
+    }
+
+
+
+}
