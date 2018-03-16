c45f67e13100d49c23476e535579f84cd1c5023e
rosvik <j.rosvik@gmail.com>
2018-03-16 12:50:25 +0100

    Added "list" command line functionality

--- src/terminalController/MainController.java
+++ src/terminalController/MainController.java
@@ -3,15 +3,14 @@ package terminalController;
 import com.util.ConnectionConfiguration;
 import main.Add;
 import main.Connect;
+import main.List;
 
 import java.sql.Connection;
 
 public class MainController {
 
-	private String command = null;
 	private String[] args;
-	Connection conn;
-
+	private Connection conn;
 
 	private MainController(Connection conn, String[] args) {
 		this.conn = conn;
@@ -30,7 +29,7 @@ public class MainController {
 				case "list":
 				case "--list":
 				case "-l":
-					System.out.println("TODO: 'List' function"); // TODO: 'List' function
+					list();
 					break;
 
 				case "connect":
@@ -61,8 +60,83 @@ public class MainController {
 		System.out.println("  connect, -c, --connect: [exercise/equipment/group] [data]");
 	}
 
-	private void connect() {
+	private void list() {
 		String usage = "Usage: " + args[0] + " [workout/exercise/equipment/group] [data]";
+		if (this.args.length == 1) {
+			System.out.println("Error: Too few arguments\n");
+			System.out.println(usage);
+		} else {
+			switch (this.args[1]) {
+				case "help":
+					// Example: --list help
+					System.out.println("List commands: workout, exercise, equipment, group");
+					System.out.println("  workout:    " + args[0] + " workout [count]");
+					System.out.println("  exercise:   " + args[0] + " exercise [start date: YYYY-MM-DD] [end date: YYYY-MM-DD]");
+					System.out.println("  equipment:  " + args[0] + " equipment [start date: YYYY-MM-DD] [end date: YYYY-MM-DD]");
+					System.out.println("  group:      " + args[0] + " group [name of group]");
+					break;
+				case "workout":
+					// Example: --list workout 5
+					usage = "Usage: " + args[0] + " workout [count]";
+					if (args.length < 3) {
+						System.out.println("Error: Too few arguments");
+						System.out.println(usage);
+					} else if(args.length > 3) {
+						System.out.println("Error: Too many arguments");
+						System.out.println(usage);
+					} else {
+						List lister = new List(conn);
+						lister.listWorkouts(Integer.parseInt(args[2]));
+					}
+					break;
+				case "exercise":
+					// Example: --list exercise 2012-04-25 2018-04-27
+					usage = "Usage: " + args[0] + " exercise [start date: YYYY-MM-DD] [end date: YYYY-MM-DD]";
+					if (args.length < 4) {
+						System.out.println("Error: Too few arguments");
+						System.out.println(usage);
+					} else if(args.length > 4) {
+						System.out.println("Error: Too many arguments");
+						System.out.println(usage);
+					} else {
+						List lister = new List(conn);
+						lister.listExercises(args[2], args[3]);
+					}
+					break;
+				case "group":
+					// Example: --list group "running"
+					usage = "Usage: " + args[0] + " group [name of group]";
+					if (args.length < 3) {
+						System.out.println("Error: Too few arguments");
+						System.out.println(usage);
+					} else if(args.length > 3) {
+						System.out.println("Error: Too many arguments");
+						System.out.println(usage);
+					} else {
+						List lister = new List(conn);
+						lister.listExercisesInGroup(args[2]);
+					}
+					break;
+				case "equipment":
+					// Example: --list equipment 2012-04-25 2018-04-27
+					usage = "Usage: " + args[0] + " equipment [start date: YYYY-MM-DD] [end date: YYYY-MM-DD]";
+					if (args.length < 4) {
+						System.out.println("Error: Too few arguments");
+						System.out.println(usage);
+					} else if(args.length > 4) {
+						System.out.println("Error: Too many arguments");
+						System.out.println(usage);
+					} else {
+						List lister = new List(conn);
+						lister.listExerciseswithEquipment(args[2], args[3]);
+					}
+					break;
+			}
+		}
+	}
+
+	private void connect() {
+		String usage = "Usage: " + args[0] + " [exercise/equipment/group] [data]";
 		if (this.args.length == 1) {
 			System.out.println("Error: Too few arguments\n");
 			System.out.println(usage);
