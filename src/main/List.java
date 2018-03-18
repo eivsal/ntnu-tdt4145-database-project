aa72ebf79421556d21f9529863ca37dd632fbe6e
Pål Fossnes <paul.fossnes@gmail.com>
2018-03-18 20:30:12 +0100

    Description of what you recieve when you are calling list-functions

--- src/main/List.java
+++ src/main/List.java
@@ -17,21 +17,29 @@ public class List {
     }
 
     public void listWorkouts (int n){
+        System.out.println("Her er de " + n +" siste treningsøktene:");
+        System.out.println("    Dato    |    Start    | Form |   Notat            | Id");
         final String sql = "SELECT * FROM workout ORDER BY date DESC, startTime DESC LIMIT " + n;
         pstatement(sql);
     }
 
     public void listExercises (String startDato, String stopDato) {
+        System.out.println("Her er resultat fra treningsøvelsene i tidsperioden " + startDato + "  -  " + stopDato + " :");
+        System.out.println("   Varighet    |   Ytelse     ");
         final String sql = "SELECT workout_has_exercise.duration, workout_has_exercise.performance, workout_has_exercise.Workout_id FROM ((workout_has_exercise INNER JOIN exercise ON workout_has_exercise.Exercise_id = exercise.id) INNER JOIN workout ON workout_has_exercise.Workout_id = workout.id) WHERE " +startDato+ "< workout.date < "+stopDato;
         pstatement(sql);
     }
 
     public void  listExercisesInGroup(String name){
+        System.out.println("Her er alle treningsøvelsene i gruppen " + name + " :");
+        System.out.println("   Id   |    Navn    |     Beskrivelse     ");
         final String sql = "SELECT exercise.id, exercise.name, exercise.description FROM ((exercise INNER JOIN exercise_has_exercisegroup ON exercise.id = exercise_has_exercisegroup.Exercise_id) INNER JOIN exercisegroup ON exercisegroup.id = exercise_has_exercisegroup.ExerciseGroup_id) WHERE exercisegroup.name = '" +name+"'";
         pstatement(sql);
     }
 
     public void listExerciseswithEquipment(String startDato, String stopDato){
+        System.out.println("Her er alle treningsøvelsene for hvert treningsapparat i tidsperioden " + startDato + "  -  " + stopDato + " :");
+        System.out.println("  Id  |    Navn    |      Beskrivelse      ");
         final String sql = "SELECT exercise.id, exercise.name, exercise.description FROM ((exercise INNER JOIN (exercise_has_equipment INNER JOIN equipment ON exercise_has_equipment.Equipment_id = equipment.id) ON exercise.id = exercise_has_equipment.Exercise_id) INNER JOIN (workout INNER JOIN workout_has_exercise ON workout.id = workout_has_exercise.Workout_id) ON workout_has_exercise.Exercise_id = exercise.id) WHERE "+startDato+ "< workout.date < "+stopDato ;
         pstatement(sql);
     }
@@ -46,9 +54,9 @@ public class List {
             while (rs.next()) {
                 for(int i = 1; i < columnsNumber + 1; i++) {
                 	if (i == columnsNumber) {
-		                System.out.print(rs.getString(i) + " ");
+		                System.out.print(rs.getString(i));
 	                } else {
-                		System.out.print(rs.getString(i) + ", ");
+                		System.out.print(rs.getString(i) + "  |   ");
 	                }
                 }
                 System.out.println();
@@ -65,11 +73,8 @@ public class List {
         Connection conn = ConnectionConfiguration.getConnection();
         Add adder = new Add(conn);
         List lister = new List(conn);
-        adder.addExercise("Løpe", "Hina deg eg løp fort");
-        adder.addExercise("Hinke", "Hina deg eg hinka fort");
-        adder.addExercise("Hoppe", "Hina deg eg hoppa fort");
-        lister.listExercises("2017-04-25","2018-06-20");
-        lister.listWorkouts(4);
+
+
     }
 
 
