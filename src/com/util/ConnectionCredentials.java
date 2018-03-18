5c6ad51a5faad38b0683237bec031419d9136aa7
rosvik <j.rosvik@gmail.com>
2018-03-18 20:33:04 +0100

    Added config-file

--- src/com/util/ConnectionCredentials.java
+++ src/com/util/ConnectionCredentials.java
@@ -1,9 +1,37 @@
 package com.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ConnectionCredentials {
-	private String user = "root";
-	private String password = "";
-	private String url = "jdbc:mysql://localhost:3306/workoutdiary";
+
+	private String user;
+	private String password;
+	private String url;
+	private List<String> list;
+
+	public ConnectionCredentials() {
+		File file = new File("config.conf");
+		list = new ArrayList<>();
+		Scanner input;
+
+		try {
+			input = new Scanner(file);
+			while (input.hasNextLine()) {
+				list.add(input.nextLine());
+			}
+
+			user = list.get(0);
+			password = list.get(1);
+			url = list.get(2);
+
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+		}
+	}
 
 	public String getPassword() {
 		return password;
